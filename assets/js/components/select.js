b92b64e737b92510bff432c3a2657261b0f77d0e
Giovanni Francischelli <gifrancischelli@gmail.com>
2023-08-24 21:19:17 +0300

    select: refactor js hook to use "this"

--- assets/js/components/select.js
+++ assets/js/components/select.js
@@ -3,45 +3,43 @@ const INPUT_EVENT = new Event("input", { bubbles: true });
 export const SelectComponent = {
   mounted() {
     const hook = this;
-    const select = this.el.querySelector("select")
-    const searchInput = this.el.querySelector("input[name='search']")
-    const listBox = this.el.querySelector("[role='listbox']")
 
-    this.select = select;
-    this.searchInput = searchInput;
+    this.select = this.el.querySelector("select")
+    this.listBox = this.el.querySelector("[role='listbox']")
+    this.searchInput = this.el.querySelector("input[name='search']")
 
-    select.addEventListener(`select-option`, (event) => {
+    this.select.addEventListener(`select-option`, (event) => {
       hook.select_option(event.detail.id);
     });
 
     this.el.addEventListener("clear-search", () => {
-      searchInput.value = ""
-      searchInput.dispatchEvent(INPUT_EVENT)
+      this.searchInput.value = ""
+      this.searchInput.dispatchEvent(INPUT_EVENT)
     });
 
-    listBox.addEventListener("mouseover", (event) => {
+    this.listBox.addEventListener("mouseover", (event) => {
       const option = event.toElement;
 
       if (option instanceof HTMLLIElement) {
-        clear_active_results(listBox)
+        this.clear_active_results()
         option.setAttribute("data-ui-active", true)
-        searchInput.setAttribute("aria-activedescendant", option.id)
+        this.searchInput.setAttribute("aria-activedescendant", option.id)
       }
     });
 
-    searchInput.addEventListener("keydown", (event) => {
+    this.searchInput.addEventListener("keydown", (event) => {
       switch (event.key) {
         case "ArrowUp":
           event.preventDefault()
-          set_next_active_option(listBox, "up", searchInput)
+          this.set_next_active_option("up")
           break;
         case "ArrowDown":
           event.preventDefault()
-          set_next_active_option(listBox, "down", searchInput)
+          this.set_next_active_option("down")
           break;
         case "Enter":
           event.preventDefault();
-          const active_option = get_active_option(listBox);
+          const active_option = this.get_active_option();
           if (active_option) hook.select_option(active_option.getAttribute("data-value"));
           break;
       }
@@ -59,33 +57,33 @@ export const SelectComponent = {
     this.searchInput.value = ""
 
     this.liveSocket.execJS(this.el, this.el.getAttribute("data-js-on-select"))
-  }
-}
+  },
+  get_active_option() {
+    return this.listBox.querySelector("[data-ui-active]");
+  },
+  clear_active_results() {
+    for (const el of this.listBox.querySelectorAll("[data-ui-active]")) {
+      el.removeAttribute("data-ui-active");
+    }
+  },
+  set_next_active_option(direction) {
+    const active_option = this.get_active_option()
 
-function get_active_option(listBox) {
-  return listBox.querySelector("[data-ui-active]")
-}
+    let new_active_option;
 
-function set_next_active_option(listBox, direction, searchInput) {
-  const active_option = get_active_option(listBox)
+    if (direction == "down") {
+      new_active_option = active_option?.nextElementSibling ||
+        this.listBox.querySelector("[role='option']:first-child")
+    }
 
-  let new_active_option;
+    if (direction == "up") {
+      new_active_option = active_option?.previousElementSibling ||
+        this.listBox.querySelector("[role='option']:last-child")
+    }
 
-  if (direction == "down") {
-    new_active_option = active_option?.nextElementSibling || listBox.querySelector("[role='option']:first-child")
-  }
+    active_option?.removeAttribute("data-ui-active")
+    new_active_option?.setAttribute("data-ui-active", true)
 
-  if (direction == "up") {
-    new_active_option = active_option?.previousElementSibling || listBox.querySelector("[role='option']:last-child")
-  }
-
-  active_option?.removeAttribute("data-ui-active")
-  new_active_option?.setAttribute("data-ui-active", true)
-  searchInput.setAttribute("aria-activedescendant", new_active_option?.id)
-}
-
-function clear_active_results(listbox) {
-  for (const el of listbox.querySelectorAll("[data-ui-active]")) {
-    el.removeAttribute("data-ui-active");
+    this.searchInput.setAttribute("aria-activedescendant", new_active_option?.id)
   }
 }
